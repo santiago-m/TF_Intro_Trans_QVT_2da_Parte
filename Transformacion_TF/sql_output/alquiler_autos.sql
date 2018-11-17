ff136b2efd3b4d7a49e59f554c1905808f4b1a7f
Santiago Maria <santimaria93@gmail.com>
2018-11-17 15:36:47 -0300

    fixed some erros in stage 2

--- Transformacion_TF/sql_output/alquiler_autos.sql
+++ Transformacion_TF/sql_output/alquiler_autos.sql
@@ -5,71 +5,55 @@ DROP TABLE IF EXISTS `relacion_nn`;
 CREATE TABLE `relacion_nn` (
 	`attr1` varchar(25),
 	`attr2` varchar(25),
-	`fk_patent_relacion_nn` varchar(25),
-	`fk_document_relacion_nn` varchar(25),
+	`document_relacion_nn` varchar(25),
+	`patent_relacion_nn` varchar(25),
 	FOREIGN KEY (
-	 fk_document_relacion_nn
+	 document_relacion_nn
 	) REFERENCES Person(
 	document
 	),
 	FOREIGN KEY (
-	 fk_patent_relacion_nn
+	 patent_relacion_nn
 	) REFERENCES Vehicle(
 	patent
 	),
-	UNIQUE (
-	attr1
-	),
-	UNIQUE (
-	attr1
-	),
-PRIMARY KEY (`fk_patent_relacion_nn`, `fk_document_relacion_nn`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `Person`;
-CREATE TABLE `Person` (
-	`attribute_2_Person` decimal(10, 2),
-	`attribute_1_Person` decimal(10, 2),
-	`multival_attr_Person` varchar(25),
-	`address` varchar(25),
-	`document` varchar(25),
-	`phones` varchar(25),
-PRIMARY KEY (`document`)
+PRIMARY KEY (`document_relacion_nn`, `patent_relacion_nn`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Client`;
 CREATE TABLE `Client` (
-	`fk_document_Client` varchar(25),
+	`document_Client` varchar(25),
 	FOREIGN KEY (
-	 fk_document_Client
+	 document_Client
 	) REFERENCES Person(
 	document
 	),
-PRIMARY KEY (`fk_document_Client`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `Partner`;
-CREATE TABLE `Partner` (
-	`car_quantity` int(25),
-	`fk_document_Partner` varchar(25),
-	FOREIGN KEY (
-	 fk_document_Partner
-	) REFERENCES Person(
-	document
-	),
-PRIMARY KEY (`fk_document_Partner`)
+PRIMARY KEY (`document_Client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Mphones_Person`;
 CREATE TABLE `Mphones_Person` (
 	`phones_Mphones_Person` varchar(25),
-	`fk_document_Mphones_Person` varchar(25),
+	`document_Mphones_Person` varchar(25),
 	FOREIGN KEY (
-	 fk_document_Mphones_Person
+	 phones_Mphones_Person
+	, 
+		 document_Mphones_Person
 	) REFERENCES Person(
 	document
 	),
-PRIMARY KEY (`phones_Mphones_Person`, `fk_document_Mphones_Person`)
+PRIMARY KEY (`phones_Mphones_Person`, `document_Mphones_Person`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `Person`;
+CREATE TABLE `Person` (
+	`address` varchar(25),
+	`attribute_1_Person` decimal(10, 2),
+	`multival_attr_Person` varchar(25),
+	`phones` varchar(25),
+	`document` varchar(25),
+	`attribute_2_Person` decimal(10, 2),
+PRIMARY KEY (`document`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Vehicle`;
@@ -78,29 +62,41 @@ CREATE TABLE `Vehicle` (
 	`kilometers` decimal(10, 2),
 	`model` int(25),
 	`patent` varchar(25),
-	`fk_fk_document_Client_Vehicle` varchar(25),
-	`fk_fk_document_Partner_Vehicle` varchar(25),
+	`document_Client_Vehicle` varchar(25),
+	`document_Partner_Vehicle` varchar(25),
 	FOREIGN KEY (
-	 fk_fk_document_Partner_Vehicle
+	 document_Partner_Vehicle
 	) REFERENCES Partner(
-	fk_document_Partner
+	document_Partner
 	),
 	FOREIGN KEY (
-	 fk_fk_document_Client_Vehicle
+	 document_Client_Vehicle
 	) REFERENCES Client(
-	fk_document_Client
+	document_Client
 	),
 PRIMARY KEY (`patent`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `Partner`;
+CREATE TABLE `Partner` (
+	`car_quantity` int(25),
+	`document_Partner` varchar(25),
+	FOREIGN KEY (
+	 document_Partner
+	) REFERENCES Person(
+	document
+	),
+PRIMARY KEY (`document_Partner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Mmultival_attr_Person`;
 CREATE TABLE `Mmultival_attr_Person` (
 	`multival_attr_Mmultival_attr_Person` varchar(25),
-	`fk_document_Mmultival_attr_Person` varchar(25),
+	`document_Mmultival_attr_Person` varchar(25),
 	FOREIGN KEY (
-	 fk_document_Mmultival_attr_Person
+	 document_Mmultival_attr_Person
 	) REFERENCES Person(
 	document
 	),
-PRIMARY KEY (`multival_attr_Mmultival_attr_Person`, `fk_document_Mmultival_attr_Person`)
+PRIMARY KEY (`multival_attr_Mmultival_attr_Person`, `document_Mmultival_attr_Person`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
