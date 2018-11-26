69b2216ad9a85a3c1d6e21a1ad9e04189879832c
Santiago Maria <santimaria93@gmail.com>
2018-11-25 23:02:45 -0300

    added https to model uri

--- Transformacion_TF/sql_output/rent_a_car.sql
+++ Transformacion_TF/sql_output/rent_a_car.sql
@@ -1,26 +1,25 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ rent_a_car;
 USE rent_a_car;
 set foreign_key_checks=0;
-DROP TABLE IF EXISTS `Client`;
-CREATE TABLE `Client` (
-	`document_Client` varchar(25),
-	`address_Client` varchar(25),
-PRIMARY KEY (`document_Client`)
+DROP TABLE IF EXISTS `Price_Scale`;
+CREATE TABLE `Price_Scale` (
+	`value` varchar(25),
+	`code` int(25),
+PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Rent`;
 CREATE TABLE `Rent` (
-	`paid_amount` decimal(10, 2),
-	`document_Client_Rent` varchar(25),
-	`price_by_kilometer` decimal(10, 2),
-	`kilometers_done` decimal(10, 2),
+	`rent_code` varchar(25),
 	`patent_Rent` varchar(25),
 	`date` datetime,
-	`rent_code` varchar(25),
+	`kilometers_done` decimal(10, 2),
+	`paid_amount` decimal(10, 2),
+	`Persons_document_in_Client_Rent` varchar(25),
 	FOREIGN KEY (
-	 `document_Client_Rent`
+	 `Persons_document_in_Client_Rent`
 	) REFERENCES `Client`(
-	`document_Client`
+	`Persons_document_in_Client`
 	),
 	FOREIGN KEY (
 	 `patent_Rent`
@@ -30,55 +29,50 @@ CREATE TABLE `Rent` (
 PRIMARY KEY (`rent_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `Client`;
+CREATE TABLE `Client` (
+	`Persons_address_in_Client` varchar(25),
+	`Persons_document_in_Client` varchar(25),
+PRIMARY KEY (`Persons_document_in_Client`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 DROP TABLE IF EXISTS `Vehicle`;
 CREATE TABLE `Vehicle` (
-	`brand` varchar(25),
-	`model` int(25),
 	`kilometers` decimal(10, 2),
 	`patent` varchar(25),
+	`brand` varchar(25),
+	`model` int(25),
 PRIMARY KEY (`patent`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `Mprice_scale_Rent`;
-CREATE TABLE `Mprice_scale_Rent` (
-	`rent_code_Mprice_scale_Rent` varchar(25),
-	`price_scale_Mprice_scale_Rent` varchar(25),
-	FOREIGN KEY (
-	 `rent_code_Mprice_scale_Rent`
-	) REFERENCES `Rent`(
-	`rent_code`
-	),
-PRIMARY KEY (`rent_code_Mprice_scale_Rent`, `price_scale_Mprice_scale_Rent`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `Mphones_Partner`;
-CREATE TABLE `Mphones_Partner` (
-	`phones_Mphones_Partner` varchar(25),
-	`document_Mphones_Partner` varchar(25),
-	FOREIGN KEY (
-	 `document_Mphones_Partner`
-	) REFERENCES `Partner`(
-	`document_Partner`
-	),
-PRIMARY KEY (`phones_Mphones_Partner`, `document_Mphones_Partner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Mphones_Client`;
 CREATE TABLE `Mphones_Client` (
-	`phones_Mphones_Client` varchar(25),
-	`document_Mphones_Client` varchar(25),
+	`document_Client` varchar(25),
+	`phones_Client` varchar(25),
 	FOREIGN KEY (
-	 `document_Mphones_Client`
+	 `document_Client`
 	) REFERENCES `Client`(
-	`document_Client`
+	`Persons_document_in_Client`
 	),
-PRIMARY KEY (`phones_Mphones_Client`, `document_Mphones_Client`)
+PRIMARY KEY (`document_Client`, `phones_Client`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Partner`;
 CREATE TABLE `Partner` (
-	`address_Partner` varchar(25),
-	`document_Partner` varchar(25),
 	`car_quantity` int(25),
-PRIMARY KEY (`document_Partner`)
+	`Persons_address_in_Partner` varchar(25),
+	`Persons_document_in_Partner` varchar(25),
+PRIMARY KEY (`Persons_document_in_Partner`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `Mphones_Partner`;
+CREATE TABLE `Mphones_Partner` (
+	`phones_Partner` varchar(25),
+	`document_Partner` varchar(25),
+	FOREIGN KEY (
+	 `document_Partner`
+	) REFERENCES `Partner`(
+	`Persons_document_in_Partner`
+	),
+PRIMARY KEY (`phones_Partner`, `document_Partner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
